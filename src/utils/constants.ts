9b8e89c0543fadc6d0c2d9252c05c6766f0d6718
manh.nguyen <manh.nguyen@manhnguyen.local>
2023-11-23 13:54:38 +0700

    fix: [TS-SalonCentre] fix build ios fails 'RCTAppDelegate.h' file not found

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
 import {Dimensions, Platform} from 'react-native';
-import {PERMISSIONS} from 'react-native-permissions';
 
 const {height, width} = Dimensions.get('window');
 
@@ -10,30 +9,6 @@ const IS_IOS = PLATFORM === 'ios';
 const IS_ANDROID = PLATFORM === 'android';
 const EMPTY_STRING = '';
 const HIT_SLOP = {top: 10, right: 10, bottom: 10, left: 10};
-
-const CAMERA_PERMISSION_STRING = Platform.select({
-  ios: PERMISSIONS.IOS.CAMERA,
-  android: PERMISSIONS.ANDROID.CAMERA,
-});
-
-const PHOTO_PERMISSION_STRING = Platform.select({
-  ios: PERMISSIONS.IOS.PHOTO_LIBRARY,
-  android: PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
-});
-
-const PICKER_METHOD = {
-  CAMERA: 'camera',
-  PHOTO: 'photo',
-};
-
-const DEFAULT_PICKER_OPTION = {
-  forceJpg: true,
-  cropping: false,
-  compressImageQuality: Platform.select({
-    ios: 0.8,
-    android: 1,
-  }),
-};
 
 const ACTION_ITEM: IActionSheetItem[] = [
   {
@@ -55,14 +30,10 @@ const ACTION_ITEM: IActionSheetItem[] = [
 ];
 export {
   ACTION_ITEM,
-  CAMERA_PERMISSION_STRING,
-  DEFAULT_PICKER_OPTION,
   EMPTY_STRING,
   HIT_SLOP,
   IS_ANDROID,
   IS_IOS,
-  PHOTO_PERMISSION_STRING,
-  PICKER_METHOD,
   SCREEN_HEIGHT,
   SCREEN_WIDTH,
 };
